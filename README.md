25050b4dd9e1acced1707223222803574447e0a7
Babarski <dspuzin@ukr.net>
2018-06-29 14:28:04 +0300

    Changes

---
+++ README.md
@@ -0,0 +1,5 @@
+
+To run project:
+run mysql,
+create database from "decision_support.sql",
+run project from "run.py" as "python run.py"
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_openid import OpenID
-from config import basedir
-from flask_login import LoginManager
-import os
+
 
 app = Flask(__name__) #create application object
 app.config.from_object('config')
